b5d5882693d36a916532824d5fd50a8ffe8df5aa
EC2 Default User <ec2-user@ip-172-31-47-110.ap-northeast-1.compute.internal>
2023-12-29 09:49:10 +0000

    First commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,49 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @list.save
+    # 4. トップ画面へリダイレクト
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render("lists/new")
+    end
+  end
+
+  def index
+    @lists=List.all
+  end
+
+  def show
+    @list=List.find(params[:id])
+  end
+
+  def edit
+    @list=List.find(params[:id])
+  end
+  
+  def update
+    list=List.find(params[:id])
+    list.update(list_params)
+    redirect_to(list_path(list.id))
+  end
+  
+  def destroy
+    list=List.find(params[:id])
+    list.destroy
+    redirect_to("/lists")
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
